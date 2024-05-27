2baa53f01f58e38e40e96a5f4cf5c684c2dc1b9f
DerickNeo <neoderick@gmail.com>
2024-05-27 21:22:17 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import CurrencyDropdown from "./components/CurrencyDropdown";
+import CurrencyContext from './contexts/CurrencyContext.js';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <CurrencyContext.Provider>
+      <h1>Currency Converter</h1>
+      <CurrencyDropdown />
+    </CurrencyContext.Provider>
+  );
+}
+
+export default App;
